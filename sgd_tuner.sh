e019dbac74c9241a0c5b66eb217aace6211ea992
dangor18 <dangordon.za@gmail.com>
2024-07-18 17:17:00 +0200

    added hpc scripts

---
+++ sgd_tuner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=a100free
+#SBATCH --partition=a100
+#SBATCH --nodes=1 --ntasks=2 --gres=gpu:ampere:1
+#SBATCH --time=48:00:00
+#SBATCH --job-name="STITCH-O Tuner SGD"
+#SBATCH --mail-user=grddan017@myuct.ac.za
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+CUDA_VISIBLE_DEVICES=$(ncvd)
+
+module load python/miniconda3-py3.9
+source activate STITCHO395
+
+python main.py --config configs/model_config_sgd.yaml --tune
+
+conda deactivate
