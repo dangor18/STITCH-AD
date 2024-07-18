a05ba60cf9f5f9f30644b3729aec7c07301799fd
dangor18 <dangordon.za@gmail.com>
2024-07-18 11:34:04 +0200

    added process_chunks.py

--- pipeline.bat
+++ pipeline.bat
@@ -1,7 +1,5 @@
 @echo off
-python data\chunker.py configs\chunk_dem_config.yaml
-python data\calc_stats.py data\dem_chunks --separate 1
-python data\train_test_split.py data\dem_chunks\
-python data\scale_data_multi.py
+python data\chunker.py configs\chunk_config.yaml
+python data\process_chunks.py data\chunks\ data\chunks\
 python data\generate_metadata.py
 pause
