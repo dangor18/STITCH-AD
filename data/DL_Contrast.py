setup for tuning and fixed scaling of noise addition

data/DL_Contrast.py
@@ -35,6 +35,7 @@ class train_dataset(Dataset):
         self.resize_dim = resize_dim
         self.simplexNoise = Simplex_CLASS()
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        self.random_erasing = transforms.RandomErasing(p=0.05, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0, inplace=False)
         
         # construct metas
         with open(meta_file, "r") as f_r:
@@ -56,6 +57,29 @@ class train_dataset(Dataset):
         plt.tight_layout()
         plt.show()
 
+    def overlay_blend(self, base, top):
+        return np.where(base > 0.5, 1 - 2*(1-base)*(1-top), 2*base*top)
+
+    def get_noise(self):
+        # add simplex noise to create pseudo abnormal sample
+        size = 256
+        h_noise = np.random.randint(128, 200)
+        w_noise = np.random.randint(128, 200)
+        start_h_noise = np.random.randint(1, size - h_noise)
+        start_w_noise = np.random.randint(1, size - w_noise)
+        noise_size = (h_noise, w_noise)
+        simplex_noise = self.simplexNoise.rand_3d_octaves((3, *noise_size), 8, 0.6)
+        
+        # Normalize simplex noise to range [0, 1]
+        simplex_noise = (simplex_noise - simplex_noise.min()) / (simplex_noise.max() - simplex_noise.min()) * 0.5
+        simplex_noise = (simplex_noise - simplex_noise.mean()) / simplex_noise.std()
+        
+        # Initialize with 0.5 (neutral gray) instead of 0
+        init_noise = np.zeros((256, 256, 3))
+        init_noise[start_h_noise: start_h_noise + h_noise, start_w_noise: start_w_noise+w_noise, :] = simplex_noise.transpose(1,2,0)
+
+        return init_noise[:, :, 0]
+    
     def __getitem__(self, index):
         input = {}
         meta = self.metas[index]
@@ -69,64 +93,60 @@ class train_dataset(Dataset):
 
         # only DEM
         if np.ndim(image) == 2:
-            image = torch.unsqueeze(torch.from_numpy(image).float(), dim=0)
+            normal_image = torch.unsqueeze(torch.from_numpy(image).float(), dim=0)
+            noise = self.get_noise()
+            noise *= np.std(image)  # change std dev to match that of orchard, as without this the noise affects some orchard patches more than others
+            img_noise = image + noise
+            #img_noise = self.overlay_blend(image, noise)
+            img_noise = torch.unsqueeze(torch.from_numpy(img_noise).float(), dim=0)
+            img_noise = self.random_erasing(img_noise)
+            img_noise.clamp(0, 1)
             self.normalize = transforms.Normalize(mean=[0.449], std=[0.226])
         else:
             dem = image[:, :, 0]
-            rgb = image[:, :, 1]
-            grey = rgb
-            #prewitt_dem = filters.prewitt(dem)
-            sobel_dem = ndimage.sobel(dem)
-            #prewitt_dem = prewitt_dem[:, :, np.newaxis]
+            grey = image[:, :, 1]
+            sobel_dem = filters.prewitt(dem)
             dem_na= dem[:, :, np.newaxis]
             sobel_dem_na = sobel_dem[:, :, np.newaxis]
             grey_na = grey[:, :, np.newaxis]
-            image = np.concatenate([dem_na, sobel_dem_na, grey_na], axis=2)
+            normal_image = np.concatenate([dem_na, sobel_dem_na, grey_na], axis=2)
+            normal_image = torch.from_numpy(normal_image).float().permute(2, 0, 1)
 
-            normal_image = torch.from_numpy(image).float().permute(2, 0, 1)
-            # normalize NORMAL image patch
-            if self.normalize:
-                normal_image = self.normalize(normal_image)
-            input.update(
-                {
-                    "filename": filename,
-                    "label": label,
-                    "normal_image": normal_image,
-                }
-            )
-            if meta.get("clsname", None):
-                input["clsname"] = meta["clsname"]
-            else:
-                input["clsname"] = filename.split("/")[-4]
-
-            # add simplex noise to create psuedo abnormal sample
-            size = 256
-            h_noise = np.random.randint(100, 200)
-            w_noise = np.random.randint(100, 200)
-            start_h_noise = np.random.randint(1, size - h_noise)
-            start_w_noise = np.random.randint(1, size - w_noise)
-            noise_size = (h_noise, w_noise)
-            simplex_noise = self.simplexNoise.rand_3d_octaves((3, *noise_size), 8, 0.5)
-            # Normalize simplex noise to range [0, 1]
-            simplex_noise = (simplex_noise - simplex_noise.min()) / (simplex_noise.max() - simplex_noise.min())
-
-            # Scale noise to a smaller range, e.g., [-0.2, 0.2]
-            simplex_noise = (simplex_noise * 0.3) - 0.2
-            init_zero = np.zeros((256,256,3))
-            init_zero[start_h_noise: start_h_noise + h_noise, start_w_noise: start_w_noise+w_noise, :] = simplex_noise.transpose(1,2,0)
-            dem_noise = dem + init_zero[:, :, 0]
-            sobel_noise = filters.sobel(dem_noise)
+            noise = self.get_noise()
+            dem_noise = noise * np.std(dem)  # change std dev to match that of orchard, as without this the noise affects some orchard patches more than others
+            dem_noise = dem + dem_noise
+            grey_noise = noise * np.std(grey)
+            grey_noise = grey + grey_noise
+            grey_na = grey_noise[:, :, np.newaxis]
+            sobel_noise = filters.prewitt(dem_noise)
             dem_na = dem_noise[:, :, np.newaxis]
             sobel_noise = sobel_noise[:, :, np.newaxis]
-            image_noise = np.concatenate([dem_na, sobel_noise, grey_na], axis=2)
-            img_noise = torch.from_numpy(image_noise).float().permute(2, 0, 1)
+            img_noise = np.concatenate([dem_na, sobel_noise, grey_na], axis=2)
+            img_noise = torch.from_numpy(img_noise).float().permute(2, 0, 1)
+            img_noise = self.random_erasing(img_noise)
             img_noise.clamp(0, 1)
             #print(simplex_noise.min(), simplex_noise.max())
-            if self.normalize:
-                img_noise = self.normalize(img_noise)
+        
+        if self.normalize:
+            img_noise = self.normalize(img_noise)
+        # normalize NORMAL image patch
+        if self.normalize:
+            normal_image = self.normalize(normal_image)
+        input.update(
+            {
+                "filename": filename,
+                "label": label,
+                "normal_image": normal_image,
+                "abnormal_image": img_noise,
+            }
+        )
+        if meta.get("clsname", None):
+            input["clsname"] = meta["clsname"]
+        else:
+            input["clsname"] = filename.split("/")[-4]
 
-            input.update({"abnormal_image": img_noise})
-
+        print(normal_image)
+        print(img_noise)
         self.plot_channels(normal_image, "Original Image Channels")
         self.plot_channels(img_noise, "Noisy Image Channels")
 