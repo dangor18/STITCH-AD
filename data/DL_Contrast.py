fixed the scaling problem of my nightmares

data/DL_Contrast.py
@@ -35,7 +35,7 @@ class train_dataset(Dataset):
         self.resize_dim = resize_dim
         self.simplexNoise = Simplex_CLASS()
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        self.random_erasing = transforms.RandomErasing(p=0.05, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0, inplace=False)
+        self.random_erasing = transforms.RandomErasing(p=0.25, scale=(0.5, 0.8), ratio=(0.3, 3.3), value=0, inplace=False)
         
         # construct metas
         with open(meta_file, "r") as f_r:
@@ -57,9 +57,6 @@ class train_dataset(Dataset):
         plt.tight_layout()
         plt.show()
 
-    def overlay_blend(self, base, top):
-        return np.where(base > 0.5, 1 - 2*(1-base)*(1-top), 2*base*top)
-
     def get_noise(self):
         # add simplex noise to create pseudo abnormal sample
         size = 256
@@ -72,7 +69,7 @@ class train_dataset(Dataset):
         
         # Normalize simplex noise to range [0, 1]
         simplex_noise = (simplex_noise - simplex_noise.min()) / (simplex_noise.max() - simplex_noise.min()) * 0.5
-        simplex_noise = (simplex_noise - simplex_noise.mean()) / simplex_noise.std()
+        #simplex_noise = (simplex_noise - simplex_noise.mean()) / simplex_noise.std()
         
         # Initialize with 0.5 (neutral gray) instead of 0
         init_noise = np.zeros((256, 256, 3))
@@ -105,7 +102,9 @@ class train_dataset(Dataset):
         else:
             dem = image[:, :, 0]
             grey = image[:, :, 1]
-            sobel_dem = filters.prewitt(dem)
+            sobel_dem = ndimage.sobel(dem)
+            sobel_dem = (sobel_dem - sobel_dem.min()) / (sobel_dem.max() - sobel_dem.min())
+
             dem_na= dem[:, :, np.newaxis]
             sobel_dem_na = sobel_dem[:, :, np.newaxis]
             grey_na = grey[:, :, np.newaxis]
@@ -113,14 +112,16 @@ class train_dataset(Dataset):
             normal_image = torch.from_numpy(normal_image).float().permute(2, 0, 1)
 
             noise = self.get_noise()
-            dem_noise = noise * np.std(dem)  # change std dev to match that of orchard, as without this the noise affects some orchard patches more than others
-            dem_noise = dem + dem_noise
-            grey_noise = noise * np.std(grey)
-            grey_noise = grey + grey_noise
-            grey_na = grey_noise[:, :, np.newaxis]
-            sobel_noise = filters.prewitt(dem_noise)
-            dem_na = dem_noise[:, :, np.newaxis]
+            dem = dem + noise
+            grey = grey + noise
+            sobel_noise = ndimage.sobel(noise)
+            sobel_noise = (sobel_noise - sobel_noise.min()) / (sobel_noise.max() - sobel_noise.min())
+            sobel_noise = sobel_dem + sobel_noise
+
+            dem_na = dem[:, :, np.newaxis]
+            grey_na = grey[:, :, np.newaxis]
             sobel_noise = sobel_noise[:, :, np.newaxis]
+
             img_noise = np.concatenate([dem_na, sobel_noise, grey_na], axis=2)
             img_noise = torch.from_numpy(img_noise).float().permute(2, 0, 1)
             img_noise = self.random_erasing(img_noise)

data/DL_RD.py
@@ -62,59 +62,6 @@ class CustomDataset(Dataset):
 
     def __len__(self):
         return len(self.metas)
-    # TODO: REMOVE THESE IF NOT USED
-    def calculate_ndvi(self, image_array):
-        """
-        Calculate NDVI from a NumPy array with shape (H, W, C) where C=3,
-        and channel 2 is NIR, channel 3 is RED.
-        
-        Args:
-        image_array (numpy.ndarray): Input image array with shape (H, W, 3)
-        
-        Returns:
-        numpy.ndarray: NDVI array with shape (H, W)
-        """
-        # extract NIR and RED spectral channels
-        nir = image_array[:, :, 1].astype(np.float32)
-        red = image_array[:, :, 2].astype(np.float32)
-        
-        numerator = nir - red
-        denominator = nir + red
-        
-        ndvi = np.divide(numerator, denominator, out=np.zeros_like(numerator), where=denominator!=0)
-        ndvi = np.clip(ndvi, 0, 1)
-        return ndvi
-
-    def detect_dem_artifacts(self, image_array, smooth_sigma=1, diff_threshold=0.2):
-        """
-        Detect potential artifacts in DEM by comparing with NDVI.
-        
-        Args:
-        image_array (numpy.ndarray): Input image array with shape (H, W, C)
-        dem_index (int): Index of DEM channel (default: 0)
-        nir_index (int): Index of NIR channel (default: 1)
-        red_index (int): Index of Red channel (default: 2)
-        smooth_sigma (float): Sigma for Gaussian smoothing (default: 1)
-        diff_threshold (float): Threshold for considering a difference significant (default: 0.2)
-        
-        Returns:
-        numpy.ndarray: Array highlighting potential artifacts
-        """
-        #ndvi = self.calculate_ndvi(image_array)
-        #dem = filters.prewitt(image_array[:, :, 0].astype(np.float32))
-        # Normalize DEM and NDVI to 0-1 range
-        #ndvi_norm = (ndvi - np.min(ndvi)) / (np.max(ndvi) - np.min(ndvi))
-        
-        # Calculate difference
-        #diff = np.abs(dem - ndvi)
-        #diff = np.clip(diff, 0, 1)
-        # Apply Gaussian smoothing to reduce noise
-        #diff_smooth = ndimage.gaussian_filter(diff, sigma=smooth_sigma)
-        #thresh = np.mean(diff_smooth) + diff_threshold * np.std(diff_smooth)
-        # Threshold the difference to highlight potential artifacts
-        #artifacts = np.where(diff_smooth > thresh, 1, 0)
-        #temp = self.threshold_and_xor(dem, ndvi)
-        #return dem
     
     def plot_channels(self, image, title):
         fig, axs = plt.subplots(1, 3, figsize=(15, 5))
@@ -143,22 +90,16 @@ class CustomDataset(Dataset):
             self.normalize = transforms.Normalize(mean=[0.449], std=[0.226])
         else:
             dem = image[:, :, 0]
+            #print(dem.min(), dem.max())
             #dem = torch.unsqueeze(torch.from_numpy(dem).float(), dim=0)
             rgb = image[:, :, 1]
-            '''h, w, c = rgb.shape
-            rgb_reshaped = rgb.reshape(-1, c)
-            
-            # Apply PCA
-            pca = PCA(n_components=1)
-            grey = pca.fit_transform(rgb_reshaped)
-            
-            # Reshape grey back to 2D and convert to PyTorch tensor
-            grey = grey.reshape(h, w)'''
             grey = rgb
+            #print(grey.min(), grey.max())
             #grey = torch.unsqueeze(torch.from_numpy(grey).float(), dim=0)
             #prewitt_dem = filters.prewitt(dem)
             sobel_dem = ndimage.sobel(dem)
-
+            sobel_dem = (sobel_dem - np.min(sobel_dem)) / (np.max(sobel_dem) - np.min(sobel_dem))
+            #print(sobel_dem.min(), sobel_dem.max())
             #prewitt_dem = prewitt_dem[:, :, np.newaxis]
             dem = dem[:, :, np.newaxis]
             sobel_dem = sobel_dem[:, :, np.newaxis]

inference.py
@@ -86,9 +86,10 @@ def get_scores(params, data_loader, device):
     
     for input in data_loader:
         patch = input['image'].to(device)
+        location = (input['x'], input['y'])
         orchard_id = input['clsname'][0]
-        if score_dict.get(orchard_id) is None:  # new orchard
-            score_dict[orchard_id] = []
+
+        score_dict[orchard_id][location] = []
         with torch.no_grad():
             if params["model_type"] == "RD":    # get score from model and add to list
                 inputs = encoder(patch)
@@ -96,7 +97,7 @@ def get_scores(params, data_loader, device):
                 anomaly_map, _ = cal_anomaly_map(inputs, outputs, patch.shape[-1], amap_mode='a', weights=params["loss_weights"])
                 anomaly_map = gaussian_filter(anomaly_map, sigma=4)
                 score = np.max(anomaly_map) + params.get("score_weight", 0) * np.mean(anomaly_map)
-                score_dict[orchard_id].append(score)
+                score_dict[orchard_id][location].append(score)
             elif params["model_type"] == "RDProj":
                 inputs = encoder(patch)
                 features = proj_layer(inputs)
@@ -104,7 +105,7 @@ def get_scores(params, data_loader, device):
                 anomaly_map, _ = cal_anomaly_map(inputs, outputs, patch.shape[-1], amap_mode='a', weights=params["loss_weights"])
                 anomaly_map = gaussian_filter(anomaly_map, sigma=4)
                 score = np.max(anomaly_map) + params.get("score_weight", 0) * np.mean(anomaly_map)
-                score_dict[orchard_id].append(score)
+                score_dict[orchard_id][location].append(score)
     
     return score_dict
 
@@ -222,14 +223,23 @@ def infer_dbscan(params, data_loader, device):
     
     dbscan = DBSCAN(eps=params["eps"], min_samples=params["min_samples"])
 
-    for orchard_id, scores in score_dict.items():
-        dbscan.fit(np.array(scores).reshape(-1, 1))     # fit the model 
-        print(orchard_id + "\n", np.unique(dbscan.labels_, return_counts=True))
+    for orchard_id, locations in score_dict.items():
+        # use input as score and x, y coordinates (3d data)
+        scores = []
+        coords = []
+        for (x, y), score_list in locations.items():
+            for score in score_list:
+                scores.append(score)
+                coords.append([x, y])
+        
+        features = np.column_stack((np.array(scores), np.array(coords)))
+        dbscan.fit(features)     # fit the model 
+        #print(orchard_id + "\n", np.unique(dbscan.labels_, return_counts=True))
         pr_dict[orchard_id] = dbscan.labels_            # get predictions
-        if np.unique(pr_dict[orchard_id], return_counts=True)[1][0] > params["min_score"]:
+        if np.sum(pr_dict[orchard_id] == -1) > params["min_score"] or len(np.unique(pr_dict[orchard_id])) > params["max_clusters"]:
             pr_dict[orchard_id] = -1
         else:
-            pr_dict[orchard_id] = 1
+            pr_dict[orchard_id] = 1 
         
     return pr_dict
 

model/resnet.py
@@ -48,13 +48,21 @@ class CrossChannelAttention(nn.Module):
         
         self.gamma = nn.Parameter(torch.zeros(1))
         
-        # Add layer normalization
         self.layer_norm = nn.LayerNorm(in_channels)
         
+        self._init_weights()
+    
+    def _init_weights(self):
+        for m in [self.query_conv, self.key_conv, self.value_conv]:
+            nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        
+        nn.init.constant_(self.gamma, 0.1)
+
     def forward(self, x):
         batch_size, C, H, W = x.size()
         
-        # Apply layer normalization
         x = x.permute(0, 2, 3, 1).contiguous()
         x = self.layer_norm(x)
         x = x.permute(0, 3, 1, 2).contiguous()
@@ -66,8 +74,6 @@ class CrossChannelAttention(nn.Module):
         value = self.value_conv(x_flat)
         
         energy = torch.bmm(query, key)
-        
-        # Apply softmax with temperature scaling
         attention = nn.functional.softmax(energy / (self.in_channels ** 0.5), dim=-1)
         
         out = torch.bmm(value, attention.permute(0, 2, 1))
@@ -87,9 +93,15 @@ class SELayer(nn.Module):
             nn.Linear(channel // reduction, channel, bias=False),
             nn.Sigmoid()
         )
+        self._init_weights()
+    
+    def _init_weights(self):
+        for m in self.fc:
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
 
     def forward(self, x):
-        b, c, w, h = x.size()
+        b, c, _, _ = x.size()
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
         return x * y.expand_as(x)
@@ -105,7 +117,14 @@ class GCModule(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(channel // reduction, channel, kernel_size=1)
         )
+        self._init_weights()
     
+    def _init_weights(self):
+        nn.init.kaiming_normal_(self.conv.weight, mode='fan_out', nonlinearity='relu')
+        for m in self.transform:
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
     def context_modeling(self, x):
         b, c, h, w = x.shape
         input_x = x
@@ -115,11 +134,22 @@ class GCModule(nn.Module):
         out = torch.matmul(input_x, context)
         out = out.reshape(b, c, 1, 1)
         return out
-    
+
     def forward(self, x):
         context = self.context_modeling(x)
         y = self.transform(context)
         return x + y
+
+class CBAM(nn.Module):
+    def __init__(self, channel, reduction=16, kernel_size=7):
+        super().__init__()
+        self.ca = ChannelAttention(channel, reduction)
+        self.sa = SpatialAttention(kernel_size)
+
+    def forward(self, x):
+        x = self.ca(x)
+        x = self.sa(x)
+        return x
 
 class ChannelAttention(nn.Module):
     def __init__(self, channel, reduction=16):
@@ -132,6 +162,12 @@ class ChannelAttention(nn.Module):
             nn.Conv2d(channel // reduction, channel, 1, bias=False)
         )
         self.sigmoid = nn.Sigmoid()
+        self._init_weights()
+    
+    def _init_weights(self):
+        for m in self.fc:
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
 
     def forward(self, x):
         avg_out = self.fc(self.avg_pool(x))
@@ -144,24 +180,17 @@ class SpatialAttention(nn.Module):
         super(SpatialAttention, self).__init__()
         self.conv = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
         self.sigmoid = nn.Sigmoid()
+        self._init_weights()
+    
+    def _init_weights(self):
+        nn.init.kaiming_normal_(self.conv.weight, mode='fan_out', nonlinearity='relu')
 
     def forward(self, x):
         avg_out = torch.mean(x, dim=1, keepdim=True)
         max_out, _ = torch.max(x, dim=1, keepdim=True)
-        out = torch.concat([avg_out, max_out], dim=1)
+        out = torch.cat([avg_out, max_out], dim=1)
         out = self.conv(out)
-        return x * self.sigmoid(out) 
-    
-class CBAM(nn.Module):
-    def __init__(self, channel, reduction=16, kernel_size=7):
-        super().__init__()
-        self.ca = ChannelAttention(channel, reduction)
-        self.sa = SpatialAttention(kernel_size)
-    
-    def forward(self, x):
-        x = self.ca(x)
-        x = self.sa(x)
-        return x
+        return x * self.sigmoid(out)
 
 class BasicBlock(nn.Module):
     expansion: int = 1
@@ -528,8 +557,6 @@ class AttnBottleneck(nn.Module):
             self.attention_block = SELayer(inplanes)
         self.downsample = downsample
         self.stride = stride
-        # Add gradient clipping
-        self.clip_value = 1.0
 
     def forward(self, x: Tensor) -> Tensor:
         if self.attention is not False: