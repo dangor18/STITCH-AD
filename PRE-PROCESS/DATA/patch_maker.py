0451f6038a0fbf15ac9ea33ce52b890b1c1309a2
Daniel Gordon <dangordon.za@gmail.com>
2024-12-11 17:14:30 +0200

    removed old patch_makers

--- PRE-PROCESS/DATA/patch_maker.py
+++ PRE-PROCESS/DATA/patch_maker.py
@@ -1,288 +1,132 @@
 import argparse
-import math
 import itertools
 import numpy as np
 import os
 import rasterio
 from rasterio.windows import Window
-from rasterio.enums import Resampling
-from rasterio.windows import from_bounds
+
+from patch_utils import getTIFDimensions, get_patch_size, reprojectTIF
 
 import re
 import shutil
-import sys
 import tqdm
 import tqdm.contrib
 import tqdm.contrib.itertools
 import yaml
 
+import multiprocessing
 import time
 
-def getTIFDimensions(file_name):
+def init_workers(SRC_LIST, mask_path, nodata_ref_path):
+    global SRC, MASK, NODATA_REF
+    SRC = []
+    for src_path, nb in SRC_LIST:
+        src_ds = rasterio.open(src_path)
+        SRC.append((src_ds, nb))
+
+    MASK = rasterio.open(mask_path)
+    NODATA_REF = rasterio.open(nodata_ref_path)
+
+def process_patch(args):
     """
-    RETURNS WIDTH AND HEIGHT FOR TIF FILE
+    PROCESS A PATCH FOR A SINGLE FILE
+    args: orchard_id, i, j, x, y, block_size_x, block_size_y, anomaly_threshold, output_dir
     """
-    with rasterio.open(file_name) as file:
-        return file.width, file.height
+    (orchard_id, i, j, x, y, block_size_x, block_size_y, anomaly_threshold, output_dir) = args
 
-def get_patch_size(temp_dir, patch_size_deg, overlap_size_deg):
-    """
-    given a patch size in degrees, calculate the number of pixels for the patch size (degrees to number of pixels x and y)
-    """
-    # all files have been clipped to the same extent and scaled to the same resolution and therefore you can read pixel size from any of the files
-    files = os.listdir(temp_dir)
-    reference = files[0]
+    # read in block from upscaled image
+    win = Window(x, y, block_size_x, block_size_y)
+    # read in block from mask
+    mask_block = MASK.read(1, window=win)
+    nodata_ref_block = NODATA_REF.read(1, window=win)
+                    
+    # ignore these regions
+    if (np.any(mask_block == 0) or np.any(nodata_ref_block == -32767)):
+        return
 
-    with rasterio.open(os.path.join(temp_dir, reference)) as src:
-        # get the pixel size in degrees
-        pixel_size_x = src.transform.a
-        pixel_size_y = src.transform.e
+    # stack each band data to block array
+    band_data_list = []
+    for src, num_bands in SRC:
+        # read within window from all the bands
+        data = [src.read(b, window=win) for b in range(1, int(num_bands) + 1)]
+        band_data_list.extend(data)
 
-        e = 10 ** -6
+    block = np.stack(band_data_list, axis=0)
+    
+    case_1_count = np.count_nonzero(mask_block == 1)
+    case_2_count = np.count_nonzero(mask_block == 2)
+    case_3_count = np.count_nonzero(mask_block == 3)
+                    
+    # check for anomaly cases
+    if case_1_count > anomaly_threshold * block_size_x * block_size_y:
+        block_file_name = os.path.join(os.path.join(output_dir, "case_1"), f"{orchard_id}_block_{i}_{j}.npy")
+    elif case_2_count > anomaly_threshold * block_size_x * block_size_y:
+        block_file_name = os.path.join(os.path.join(output_dir, "case_2"), f"{orchard_id}_block_{i}_{j}.npy")
+    elif case_3_count > anomaly_threshold * block_size_x * block_size_y:
+        block_file_name = os.path.join(os.path.join(output_dir, "case_3"), f"{orchard_id}_block_{i}_{j}.npy")
+    elif case_1_count + case_2_count + case_3_count == 0:
+        block_file_name = os.path.join(os.path.join(output_dir, "normal"), f"{orchard_id}_block_{i}_{j}.npy")
+    else:
+        return
+                            
+    np.save(block_file_name, block.transpose(1, 2, 0))
 
-        # calculate the number of pixels for the patch size
-        patch_size_x = int(patch_size_deg / pixel_size_x) * e
-        patch_size_y = int(patch_size_deg / pixel_size_y) * e
-        overlap_size_x = int(overlap_size_deg / pixel_size_x) * e
-        overlap_size_y = int(overlap_size_deg / pixel_size_y) * e
-
-        return int(patch_size_x), abs(int(patch_size_y)), int(overlap_size_x), abs(int(overlap_size_y))
-
-def clip_then_upscale(files, upscale_width, upscale_height, output_dir, reference, verbose=False):
-    """
-    CLIP RASTERS WITH RESPECT TO THE EXTENT WITH RESPECT TO ... AND THEN UPSCALE ALL RASTERS TO THE SAME SIZE AND WRITE TO TEMP
-    """
-    os.makedirs(output_dir, exist_ok=True)
-    for file_name in tqdm.tqdm(files, desc="Upscaling files", disable=verbose):
-        local_name = file_name.split("/")[-1]
-        if verbose:
-            print(os.path.join(output_dir, local_name))
-        if (not os.path.exists(os.path.join(output_dir, local_name))):
-            # open raster
-            with rasterio.open(file_name) as src:
-                # open raster you're going reference to clip and load bounds
-                with rasterio.open(reference) as target:
-                    target_bounds = target.bounds
-                
-                # create window matching bounds
-                window = from_bounds(
-                    target_bounds.left, target_bounds.bottom, 
-                    target_bounds.right, target_bounds.top, 
-                    transform=src.transform
-                )
-                
-                # read from file within the window
-                data = src.read(window=window)
-                window_transform = src.window_transform(window)
-                
-                # update metadata
-                out_meta = src.meta.copy()
-                out_meta.update({
-                    "driver": "GTiff",
-                    "height": data.shape[1],
-                    "width": data.shape[2],
-                    "transform": window_transform,
-                    "crs": src.crs
-                })
-
-                # write out clipped raster
-                temp_path = os.path.join(output_dir, "temp_clipped_raster.tif")
-                with rasterio.open(temp_path, "w", **out_meta) as dest:
-                    dest.write(data)
-
-
-                    src.close()
-                    dest.close()
-            
-            
-                    src.close()
-                    dest.close()
-            
-            # read newly written clipped raster
-            with rasterio.open(temp_path) as src:
-                nodata = src.nodata
-                
-                color_interps = src.colorinterp
-
-                # upscale to match width and height
-                data = src.read(
-                    out_shape=(
-                        src.count,
-                        upscale_height,
-                        upscale_width
-                    ),
-                    resampling=Resampling.bilinear
-                )
-
-                # adjust metadata
-                transform = src.transform * src.transform.scale(
-                    (src.width / data.shape[-1]),
-                    (src.height / data.shape[-2])
-                )
-
-                output_path = os.path.join(output_dir, os.path.basename(file_name))
-                with rasterio.open(
-                    output_path,
-                    'w',
-                    driver='GTiff',
-                    height=upscale_height,
-                    width=upscale_width,
-                    count=src.count,
-                    dtype=data.dtype,
-                    crs=src.crs,
-                    transform=transform,
-                    nodata=nodata
-                ) as dst:
-                    dst.write(data)
-                    dst.colorinterp = color_interps
-        else:
-            if verbose:
-                print(f"{file_name} ALREADY EXISTS IN FOLDER")
-        
-def create_blocks_upscaling(orchard_id, files, dimensions, upscale_width, upscale_height, block_size, overlap, anomaly_threshold, output_dir, mask_file="mask.tif", reference="reg.tif",  verbose=False):
+def create_patches(orchard_id, files, dimensions, scaled_width, scaled_height, block_size, overlap, anomaly_threshold, output_dir, mask_file="mask.tif", reference="reg.tif",  verbose=False):
     """
     CREATES BLOCK BIN FILES FOR VARIOUS DIMENSIONS
     PARAMETERS: Root path to files(for block naming), List of file names, list of bands for each file, desired width and height to upscale to, block size, block overlap, output directory for chunks
     """
+    SRC_LIST = []
     # make directories
-    case_1_dir = os.path.join(output_dir, "case_1")
-    case_2_dir = os.path.join(output_dir, "case_2")
-    case_3_dir = os.path.join(output_dir, "case_3")
-    normal_dir = os.path.join(output_dir, "normal")
-    os.makedirs(case_1_dir, exist_ok=True)
-    os.makedirs(case_2_dir, exist_ok=True)
-    os.makedirs(case_3_dir, exist_ok=True)
-    os.makedirs(normal_dir, exist_ok=True)
-    # upscale all tif images
+    output_subdirs = ["case_1", "case_2", "case_3", "normal"]
+    for subdir in output_subdirs:
+        os.makedirs(os.path.join(output_dir, subdir), exist_ok=True)
+    
+    # reproject all bands
     temp_dir = os.path.join(output_dir, "temp/")
+    # include the reference TIF and the mask file in the list of files to reproject
     files.append(mask_file), files.append(reference)
     files = list(map(lambda f: f.replace('\\', '/'), files))
-    clip_then_upscale(files, upscale_width, upscale_height, temp_dir, reference=reference, verbose=verbose)
+    reprojectTIF(files, scaled_width, scaled_height, temp_dir, reference=reference, verbose=verbose)
 
     block_size_x, block_size_y, overlap_x, overlap_y = get_patch_size(temp_dir, block_size, overlap)
-    print (f"Block size: {block_size_x}x{block_size_y} px, Overlap: {overlap_x}x{overlap_y} px")
+    print(f"Block size: {block_size_x}x{block_size_y} px, Overlap: {overlap_x}x{overlap_y} px")
 
-    # open mask layer
-    mask = rasterio.open(os.path.join(temp_dir, os.path.basename(mask_file)))
-
-    # open the nodata reference file
-    nodata_ref = rasterio.open(os.path.join(temp_dir, os.path.basename(reference)))
+    mask_path = os.path.join(temp_dir, os.path.basename(mask_file))
+    nodata_ref_path = os.path.join(temp_dir, os.path.basename(reference))
 
     for file_name, num_bands in zip(files, dimensions):
         local_name = file_name.split("/")[-1]
-        with rasterio.open(os.path.join(temp_dir, local_name)) as src:
-            # block starting positions
-            y_starts = np.arange(0, upscale_height - block_size_y + 1, block_size_y - overlap_y)
-            x_starts = np.arange(0, upscale_width - block_size_x + 1, block_size_x - overlap_x)
+        src_path = os.path.join(temp_dir, local_name)
+        SRC_LIST.append((src_path, num_bands))
+        
+    # block starting positions
+    y_starts = np.arange(0, scaled_height - block_size_y + 1, block_size_y - overlap_y)
+    x_starts = np.arange(0, scaled_width - block_size_x + 1, block_size_x - overlap_x)
 
-            # determines whether to show progress bar or not. Don't show if verbose is enabled since the print statements get in the way of the progress bar
-            if verbose:
-                loop_product = itertools.product(enumerate(y_starts), enumerate(x_starts))
-            else:
-                loop_product = tqdm.contrib.itertools.product(enumerate(y_starts), enumerate(x_starts), total=len(y_starts) * len(x_starts), desc=f"Processing {local_name}")
-            
-            for (i, y), (j, x) in loop_product:
-                # read in block from upscaled image
-                win = Window(x, y, block_size_x, block_size_y)
-                # read in block from mask
-                mask_block = mask.read(1, window=win)
-                nodata_ref_block = nodata_ref.read(1, window=win)
-                    
-                # ignore these regions
-                if (not np.any(mask_block == 0)) and (not np.any(nodata_ref_block == -32767)):
-                    # stack each band data to block array
-                    block = np.stack([src.read(b, window=win) for b in range(1, num_bands + 1)])
-                    
-                    case_1_count = np.count_nonzero(mask_block == 1)
-                    case_2_count = np.count_nonzero(mask_block == 2)
-                    case_3_count = np.count_nonzero(mask_block == 3)
-                    
-                    # check for anomaly cases
-                    if case_1_count > anomaly_threshold * block_size_x * block_size_y:
-                        block_file_name = os.path.join(case_1_dir, f"{orchard_id}_block_{i}_{j}.npy")
-                    elif case_2_count > anomaly_threshold * block_size_x * block_size_y:
-                        block_file_name = os.path.join(case_2_dir, f"{orchard_id}_block_{i}_{j}.npy")
-                    elif case_3_count > anomaly_threshold * block_size_x * block_size_y:
-                        block_file_name = os.path.join(case_3_dir, f"{orchard_id}_block_{i}_{j}.npy")
-                    elif case_1_count + case_2_count + case_2_count == 0:
-                        block_file_name = os.path.join(normal_dir, f"{orchard_id}_block_{i}_{j}.npy")
-                    else:
-                        continue
+    loop_product = itertools.product(enumerate(y_starts), enumerate(x_starts))
+    patch_args = []
+    # extract patches for a channel in parallel
+    for (i, y), (j, x) in loop_product:
+        patch_args.append((
+            orchard_id,
+            i,
+            j,
+            x,
+            y,
+            block_size_x,
+            block_size_y,
+            anomaly_threshold,
+            output_dir
+        ))
 
-                    # check if block file exists and if so then append to data and write
-                    if os.path.exists(block_file_name):
-                        temp_block_data = np.load(block_file_name)
-                        temp_block_data = temp_block_data.transpose(2, 0, 1)
-                        updated_block = np.concatenate((temp_block_data, block), axis=0)
-                    else:
-                        # initialize file if block doesn't exist
-                        updated_block = block
-                            
-                    updated_block = updated_block.transpose(1, 2, 0)
-                    np.save(block_file_name, updated_block)
-                    if verbose:
-                        print(f'SAVED BLOCK {i}, {j} TO {block_file_name}')
-                else:
-                    if verbose:
-                        print(f'SKIPPING BLOCK {i}, {j}')
+    with multiprocessing.Pool(processes=os.cpu_count(),
+                                initializer=init_workers, 
+                                initargs=(SRC_LIST, mask_path, nodata_ref_path)) as pool:
+        pool.map(process_patch, patch_args)
     
     # delete temp upscale tif folder and contents
-    mask.close()
-    nodata_ref.close()
-    src.close()
     shutil.rmtree(temp_dir)
-
-def test_upscaling(files, upscale_width, upscale_height, output_dir):
-    """
-    TEST UPSCALING OF TIF IMAGES
-    """
-    os.makedirs(output_dir, exist_ok=True)
-
-    for file_name in files:
-        with rasterio.open(file_name) as src:
-            # print(f"UPSCALING {file_name} WITH DIMENSIONS: WIDTH {src.width}, HEIGHT {src.height} TO: WIDTH {upscale_width}, HEIGHT {upscale_height}")
-
-            nodata = src.nodata
-            color_interps = src.colorinterp
-
-            data = src.read(
-                out_shape=(
-                    src.count,
-                    upscale_height,
-                    upscale_width
-                ),
-                resampling=Resampling.bilinear
-            )
-
-            # adjust metadata
-            transform = src.transform * src.transform.scale(
-                (src.width / data.shape[-1]),
-                (src.height / data.shape[-2])
-            )
-
-            output_path = os.path.join(output_dir, os.path.basename(file_name))
-            with rasterio.open(
-                output_path,
-                'w',
-                driver='GTiff',
-                height=upscale_height,
-                width=upscale_width,
-                count=src.count,
-                dtype=data.dtype,
-                crs=src.crs,
-                transform=transform,
-                nodata=nodata
-            ) as dst:
-                dst.write(data)
-                dst.colorinterp = color_interps
-
-def get_files_from_project(project_dir: str) -> list[str]:
-    return [project_dir + '/orthos/' + f for f in [
-        'data-analysis/lwir.tif',
-        'data-analysis/red.tif',
-        'data-analysis/reg.tif',
-        'export-data/orthomosaic_visible.tif'
-    ]]
 
 def main():
     parser = argparse.ArgumentParser(description="A tool for chunking large orthomosaic TIF files into smaller patches.")
@@ -295,12 +139,7 @@ def main():
     config_path = os.path.join(cwd, args.config)
     verbose = args.verbose
 
-    if not os.path.exists(config_path):
-        print(f"Config file {config_path} does not exist.")
-        print("Usage: python chunker.py [config_file_path]")
-        exit()
-
-
+    # try open config file
     with open(config_path, "r") as f:
         config_documents = yaml.safe_load_all(f)
         for _, config in enumerate(config_documents):
@@ -312,14 +151,12 @@ def main():
                 for file in config["files"]:
                     files.append(os.path.join(path, file["name"]))
                     dimensions.append(file["dimensions"])
+
                 chunk_size = config["chunk_size"]
                 chunk_overlap = config["chunk_overlap"]
-                # this represents the percentage overlap a chunk needs to have with an anomalous region to be considered anomalous
-                #normal_threshold = config["normal_threshold"]
                 anomaly_threshold = config["anomaly_threshold"]
-                
                 scale_ratio = config["scale_ratio"]
-                
+                    
                 # get width and height of RGB file for orchard and times each by scale factor (new height and width of each channel for this particular orchard)
                 rgb_path = os.path.join(path, "orthos", "export-data", "orthomosaic_visible.tif")
                 width, height = getTIFDimensions(rgb_path)
@@ -329,8 +166,7 @@ def main():
 
                 if verbose:
                     print(f"Scaling every channel to: {scale_width}x{scale_height}")
-                
-                # mask used to hide outer boundary and unwanted regions (buildings, etc.). Also used to determine which blocks are normal, case 1, or case 2 anomalies
+                    
                 mask_file = config["mask"]
                 # reference file used to determine nodata regions and clip the rasters against before upscaling
                 reference_file = config["reference"]
@@ -340,10 +176,10 @@ def main():
                 field = re.findall(r"'(.+?)'", str(e))[-1]
                 print(f"Missing required field '{field}' in .yaml configuration document.")
                 exit()
-
+                
             print(f"Chunking project: {path}")
-            orchard_id = path.split(os.path.sep)[-1]
-            create_blocks_upscaling(orchard_id, files, dimensions, scale_width, scale_height, chunk_size, chunk_overlap, anomaly_threshold, output_dir, mask_file, reference_file, verbose=verbose)
+            orchard_id = os.path.basename(path)
+            create_patches(orchard_id, files, dimensions, scale_width, scale_height, chunk_size, chunk_overlap, anomaly_threshold, output_dir, mask_file, reference_file, verbose=verbose)
 
 if __name__ == "__main__":
     try:
