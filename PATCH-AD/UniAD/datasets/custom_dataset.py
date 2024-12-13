a9441b4a065819e19f66920a13400bfeb047d2ba
Joe Whales <joewhales0@gmail.com>
2024-12-13 12:47:06 +0200

    Added scaling to UniAD custom dataset

--- PATCH-AD/UniAD/datasets/custom_dataset.py
+++ PATCH-AD/UniAD/datasets/custom_dataset.py
@@ -186,8 +186,8 @@ class CustomDataset(BaseDataset):
             else:
                 raise ValueError("Labels must be [None, 0, 1]!")
 
-        # Scale image to lie between 0 and 255
-        # image = (image - image.min()) / (image.max() - image.min()) * 255
+        # Scale image to lie between its min and max values
+        image = (image - image.min()) / (image.max() - image.min())
 
         # convert image to tensor and permute
         image = from_numpy(image).float()
